1328f45a17664f0b9e70bc0695fd91e1cba79a4c
folioportfolio <nici@hotmail.cz>
2025-12-21 18:15:26 +0100

    Improved List View

--- components/views/RenderBrowserView.tsx
+++ components/views/RenderBrowserView.tsx
@@ -1,10 +1,10 @@
-import { Text, StyleSheet, ScrollView, View, Platform, RefreshControl } from "react-native";
-import RenderInfo from "../RenderInfo";
+import { Text, StyleSheet, ScrollView, View, RefreshControl, SectionList, SectionListData } from "react-native";
 import { useRenderJobs } from "../../contexts/renderContext";
-import { RenderJob } from "../../types/types";
-import Card from "../controls/Card";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useMemo, useState } from "react";
+import RenderInfo from "../RenderInfo";
+import Card from "../controls/Card";
+import { RenderJob } from "../../types/types";
 
 export default function RenderBrowserView() {
     const renderContext = useRenderJobs();
@@ -22,13 +22,54 @@ export default function RenderBrowserView() {
     const runningStates = ["inProgress", "started"];
     const doneStates = ["finished", "canceled"]
 
-    const inProgressJobs = allJobs.filter(x => runningStates.includes(x.state));
-    const finishedJobs = allJobs.filter(x => doneStates.includes(x.state));
+    const inProgressJobs: RenderJob[] = allJobs.filter(x => runningStates.includes(x.state));
+    const finishedJobs: RenderJob[] = allJobs.filter(x => doneStates.includes(x.state));
+
+    const items = [
+        {
+            title: "In Progress",
+            data: inProgressJobs
+        },
+        {
+            title: "Finished",
+            data: finishedJobs
+        }
+    ];
+
+    const getHeader = ({section}: {section: { title: string, data: RenderJob[] }}): React.ReactElement | null => {
+        if (section.data.length <= 0)
+            return null;
+
+        return (
+            <Card style={styles.headerCard}>
+                <Text style={styles.header}>{section.title}</Text>
+            </Card>
+        );
+    }
+
+    const getRenderItem = ({ item }: { item: RenderJob }): React.ReactElement | null  => {
+        return (
+            <RenderInfo id={item.id}
+                        finished={doneStates.includes(item.state)}
+                        canceled={item.state === "canceled"}
+                        state={item.state}
+                        currentFrame={item.currentFrame}
+                        frameEnd={item.frameEnd}
+                        frameStart={item.frameStart}
+                        timeStart={item.timeStart}
+                        timeEnd={item.timeLastFrame}
+                        project={item.project} />
+        );
+    }
 
     return (
         <>
             <View style={{ flex: 1 }}>
                 <SafeAreaView style={{ flex: 1 }} edges={{bottom: "off", top: "maximum"}}>
+                    <SectionList sections={items}
+                                 keyExtractor={x => x.id}
+                                 renderItem={getRenderItem}
+                                 renderSectionHeader={getHeader} />
                     <ScrollView refreshControl={
                         <RefreshControl refreshing={refreshing}
                                         onRefresh={async () => {
@@ -37,47 +78,6 @@ export default function RenderBrowserView() {
                                             setRefreshing(false);
                                         }}/>
                         }>
-                        {inProgressJobs.length > 0 &&
-                            <View>
-                                <Card style={styles.headerCard}>
-                                    <Text style={styles.header}>Renders in progress</Text>
-                                </Card>
-                                {inProgressJobs.map(job => (
-                                    <RenderInfo key={job.id}
-                                        id={job.id}
-                                        finished={false}
-                                        canceled={false}
-                                        state={job.state}
-                                        currentFrame={job.currentFrame}
-                                        frameEnd={job.frameEnd}
-                                        frameStart={job.frameStart}
-                                        timeStart={job.timeStart}
-                                        timeEnd={job.timeLastFrame}
-                                        project={job.project} />
-                                ))}
-                            </View>
-                        }
-
-                        {finishedJobs.length > 0 &&
-                            <View>
-                                <Card style={styles.headerCard}>
-                                    <Text style={styles.header}>Past renders</Text>
-                                </Card>
-                                {finishedJobs.map(job => (
-                                    <RenderInfo key={job.id}
-                                        id={job.id}
-                                        finished={true}
-                                        canceled={job.state === "canceled"}
-                                        state={job.state}
-                                        currentFrame={job.currentFrame}
-                                        frameEnd={job.frameEnd}
-                                        frameStart={job.frameStart}
-                                        timeStart={job.timeStart}
-                                        timeEnd={job.timeLastFrame}
-                                        project={job.project} />
-                                ))}
-                            </View>
-                        }
                     </ScrollView>
                 </SafeAreaView>
             </View>
